cdc570c9296aceef06daf4b66a3ff9e61fade754
Bethek-Iyke <talk2bethel017@gmail.com>
2023-06-15 18:22:54 +0100

    nodejs

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -4,11 +4,14 @@ const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
 const DATABASE = args[0];
-const port = 1245;
+
 const app = express();
+const port = 1245;
+
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
+
 app.get('/students', async (req, res) => {
   const msg = 'This is the list of our students\n';
   try {
@@ -18,37 +21,9 @@ app.get('/students', async (req, res) => {
     res.send(`${msg}${error.message}`);
   }
 });
+
 app.listen(port, () => {
   //   console.log(`Example app listening at http://localhost:${port}`);
 });
+
 module.exports = app;
-
-//const express = require('express');
-
-//const args = process.argv.slice(2);
-//const countStudents = require('./3-read_file_async');
-
-//const DATABASE = args[0];
-
-//const app = express();
-//const port = 1245;
-
-//app.get('/', (req, res) => {
-//  res.send('Hello Holberton School!');
-//});
-
-//app.get('/students', async (req, res) => {
-//  const msg = 'This is the list of our students\n';
-//  try {
-//    const students = await countStudents(DATABASE);
-//    res.send(`${msg}${students.join('\n')}`);
-//  } catch (error) {
-//    res.send(`${msg}${error.message}`);
-//  }
-//});
-
-//app.listen(port, () => {
-//  //   console.log(`Example app listening at http://localhost:${port}`);
-//});
-
-// module.exports = app;
